3ff76e11190654fa01355363277bcfb81421e1f6
gamer-2000 <fotbol668@gmail.com>
2026-03-01 16:11:24 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,16 @@
-const mineflayer = require('mineflayer')
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('AFK Bot is running.')
+})
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`)
+})
+  const mineflayer = require('mineflayer')
 
 function createBot() {
   const bot = mineflayer.createBot({
