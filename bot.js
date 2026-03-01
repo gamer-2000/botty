e2b216deefdc491bf37d2cc0881848d8d0321e58
gamer-2000 <fotbol668@gmail.com>
2026-03-01 15:43:01 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,36 +2,49 @@ const mineflayer = require('mineflayer')
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: process.env.MC_HOST,
+    host: 'bbcv2.progamer.me',
     port: 25565,
-    username: process.env.MC_USER,
-    password: process.env.MC_PASS || undefined,
+    username: 'AFK-BOT',
     version: false
   })
 
   bot.on('spawn', () => {
-    console.log('Bot joined!')
+    console.log('Bot joined server.')
 
-    setTimeout(() => {
-      bot.chat('/register yourpass yourpass')
-      bot.chat('/login yourpass')
-    }, 4000)
-
-    bot.setControlState('forward', true)
-
-    // Optional anti-AFK jump
-    setInterval(() => {
-      bot.setControlState('jump', true)
-      setTimeout(() => bot.setControlState('jump', false), 500)
-    }, 15000)
+    startMovement(bot)
   })
 
   bot.on('end', () => {
-    console.log('Bot disconnected. Reconnecting...')
+    console.log('Disconnected. Reconnecting in 10s...')
     setTimeout(createBot, 10000)
   })
 
   bot.on('error', err => console.log(err))
+}
+
+function startMovement(bot) {
+  // Move forward
+  bot.setControlState('forward', true)
+
+  // Random anti-AFK behavior
+  setInterval(() => {
+    // Random jump
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 500)
+
+    // Slight random turn
+    const yaw = bot.entity.yaw + (Math.random() - 0.5) * 2
+    bot.look(yaw, bot.entity.pitch, true)
+
+    // Random stop/start to look human
+    if (Math.random() > 0.7) {
+      bot.setControlState('forward', false)
+      setTimeout(() => {
+        bot.setControlState('forward', true)
+      }, 3000)
+    }
+
+  }, 20000)
 }
 
 createBot()
